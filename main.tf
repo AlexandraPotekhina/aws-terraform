4b9c90d52c0fcd633740459820a89b222e06cd10
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-06-23 23:39:40 +0000

    add vpc and subnet to infrastructure

--- main.tf
+++ main.tf
@@ -16,11 +16,29 @@ provider "aws" {
   # token = var.aws_token
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "default" {
   ami           = "ami-0914547665e6a707c"
   instance_type = "t3.micro"
 
   tags = {
     Name = "ServerInstance"
+  }
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "default-vpc"
+  }
+}
+
+resource "aws_subnet" "default" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-north-1"
+
+  tags = {
+    Name = "default-subnet"
   }
 }
